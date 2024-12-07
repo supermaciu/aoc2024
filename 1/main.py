8e4a4183ce89270cab93981364effcd9c655131b
supermaciu <m.jankowski2605@gmail.com>
2024-12-07 18:52:56 +0100

    2 2 star

--- 1/main.py
+++ 1/main.py
@@ -12,9 +12,17 @@ with open("data.txt", "r") as f:
 
 n1.sort()
 n2.sort()
-
-diff_sum = 0
+r = 0
+count = 0
 for i in range(len(n1)):
-    diff_sum += abs(n1[i] - n2[i])
+    if not (i-1 >= 0 and n1[i-1] == n1[i]):
+        count = 0
+        for j in range(len(n2)):
+            if n1[i] == n2[j]:
+                count += 1
+            elif n1[i] != n2[j] and count != 0:
+                break
+    
+    r += n1[i] * count
 
-print(diff_sum)
+print(r)
--- 2/main.py
+++ 2/main.py
@@ -11,19 +11,38 @@ safe_reports = 0
 for report in reports:
     safe = True
     removed = False
-    last = report[0] - report[1]
-    for i in range(len(report)-1):
-        d = report[i] - report[i+1]
-        if not (d != 0 and d/abs(d) == last/abs(last) and abs(d) >= 1 and abs(d) <= 3):
-            if removed:
-                safe = False
-                last = d
-                break
-            else:
+    i = 0; j = 1
+    last = report[j] - report[i]
+    if not (abs(last) >= 1 and abs(last) <= 3):
+        removed = True
+        d20 = report[2] - report[0]
+        d21 = report[2] - report[1]
+        if abs(d20) >= 1 and abs(d20) <= 3:
+            j = 2
+        elif abs(d21) >= 1 and abs(d21) <= 3:
+            i = 1
+            j = 2
+        else:
+            safe = False
+        last = report[j] - report[i]
+
+    while i in range(len(report)-1) and j in range(1, len(report)) and safe:
+        d = report[j] - report[i]
+
+        if not (abs(d) >= 1 and abs(d) <= 3 and d/abs(d) == last/abs(last)):
+            if not removed:
                 removed = True
+                j += 1
+                continue
+            else:
+                safe = False
+                break
+        
+        last = d if d != 0 else last
+        i += 2 if j-i == 2 else 1
+        j += 1
 
     print(report, safe)
-
     safe_reports += 1 if safe else 0
 
 print(safe_reports)
